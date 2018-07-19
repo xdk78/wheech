01e58dff0ecb01132a412a041fa582807fe46482
Filip Grzywok <rj23@protonmail.com>
2018-07-19 23:07:07 +0200

    Fix fix fix fix

--- src/IPhysics.ts
+++ src/IPhysics.ts
@@ -1,3 +1,3 @@
 export default interface IPhysics {
-  update()
+  update(): void
 }
--- src/Scene.ts
+++ src/Scene.ts
@@ -8,6 +8,7 @@ export default class Scene implements IScene {
   physics: IPhysics
 
   constructor(background: any, physics: IPhysics, sprites: Sprite[]) {
+    this.physics = physics
     this.background = background
     this.sprites = sprites
   }
