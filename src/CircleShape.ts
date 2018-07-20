1fef05fc6a1bc6d64c9372476b1da93dfd2603b0
xdk78 <xdk78@interia.pl>
2018-07-20 22:27:11 +0200

    feat: basic circle rotation

--- src/CircleShape.ts
+++ src/CircleShape.ts
@@ -7,14 +7,16 @@ export default class CircleShape implements IShape {
   protected _startAngle: number
   protected _endAngle: number
   protected _anticlockwise?: boolean
+  protected _degrees?: number
   protected _style?: any
 
-  constructor(position: Vector, radius: number, startAngle: number, endAngle: number, anticlockwise?: boolean, style?) {
+  constructor(position: Vector, radius: number, startAngle: number, endAngle: number, anticlockwise?: boolean, degrees?: number, style?) {
     this._position = position
     this._radius = radius
     this._startAngle = startAngle
     this._endAngle = endAngle
     this._anticlockwise = anticlockwise
+    this._degrees = degrees
     this._style = style
   }
 
@@ -22,10 +24,15 @@ export default class CircleShape implements IShape {
     this._position = position
   }
 
+  public setRotation(degrees: number): void {
+    this._degrees = degrees
+  }
+
   public draw(context: CanvasRenderingContext2D): void {
     const ctx = context
     ctx.beginPath()
     ctx.arc(this._position.x, this._position.y, this._radius, this._startAngle, this._endAngle, this._anticlockwise)
+    ctx.rotate((this._degrees * Math.PI) / 180)
     ctx.fillStyle = this._style.fillStyle
     ctx.fill()
     ctx.closePath()
