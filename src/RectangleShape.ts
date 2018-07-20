54cc795ce178763723a88d272df2978ad22627ee
xdk78 <xdk78@interia.pl>
2018-07-20 22:40:07 +0200

    feat: basic image support

--- src/RectangleShape.ts
+++ src/RectangleShape.ts
@@ -2,17 +2,19 @@ import IShape from './IShape'
 import { Vector } from 'matter-js'
 
 export default class RectangleShape implements IShape {
-  protected _width: number
-  protected _style?: any
-  protected _height: number
   protected _position: Vector
+  protected _width: number
+  protected _height: number
   protected _angle: number
+  protected _style?: any
+  protected _image?: any
 
-  constructor(position: Vector, width: number, height: number, style?) {
+  constructor(position: Vector, width: number, height: number, style?, image?) {
     this._position = position
     this._width = width
     this._height = height
     this._style = style
+    this._image = image
   }
 
   public setPosition(position: Vector): void {
@@ -24,6 +26,7 @@ export default class RectangleShape implements IShape {
     ctx.beginPath()
     ctx.rect(this._position.x, this._position.y, this._width, this._height)
     ctx.fillStyle = this._style.fillStyle
+    ctx.drawImage(this._image, this._width, this._height)
     ctx.fill()
     ctx.closePath()
   }
