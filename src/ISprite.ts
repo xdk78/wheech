0cdf54a481ff90358ae2b71093ff15968acc93d6
Filip Grzywok <rj23@protonmail.com>
2018-07-26 21:45:35 +0200

    Added blocks destroying

--- src/ISprite.ts
+++ src/ISprite.ts
@@ -3,4 +3,6 @@ import IShape from './IShape'
 export default interface ISprite {
   render(context: CanvasRenderingContext2D, width: number, height: number): void
   setShape(shape: IShape): void
+  isDestroyed: boolean
+  destroy()
 }
